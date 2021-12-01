2f1c8e27fbf9812588e2f619b84ebc1b3ab4cac2
ViperWR <roetsjwr@gmail.com>
2021-12-01 09:23:36 +0200

    POE

--- GADE POE/Form1.cs
+++ GADE POE/Form1.cs
@@ -1,16 +1,8 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
-using System.Runtime.Serialization;
-using System.Runtime.Serialization.Formatters.Binary;
 
 namespace GADE_POE_task_1
 {
